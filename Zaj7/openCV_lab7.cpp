6e9c408f4bf857b6556da63c5a0beac282f8daed
MikolajNoga <66532479+MikolajNoga@users.noreply.github.com>
2022-01-26 00:09:48 +0100

    lab7 created

---
+++ Zaj7/openCV_lab7.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+
+    bool capturing = true;
+    cv::VideoCapture capture(0);
+    if (!capture.isOpened()) {
+        cerr << "error while opening" << endl;
+        return -1;
+    }
+
+    int lowRanges[3] = { 0,10,60 };
+    int upRanges[3] = { 20,150,255 };
+
+    namedWindow("RangeHSV", WINDOW_AUTOSIZE);
+    namedWindow("HSV", WINDOW_AUTOSIZE);
+
+    createTrackbar("Lower Hue", "RangeHSV", &lowRanges[0], 180);
+    createTrackbar("Upper Hue", "RangeHSV", &upRanges[0], 180);
+
+    createTrackbar("Lower Sat", "RangeHSV", &lowRanges[1], 255);
+    createTrackbar("Upper Sat", "RangeHSV", &upRanges[1], 255);
+
+    createTrackbar("Lower Val", "RangeHSV", &lowRanges[2], 255);
+    createTrackbar("Upper Val", "RangeHSV", &upRanges[2], 255);
+
+    do {
+        Mat frame, baseWindow, hsvRange, HSV;
+        capture >> frame;
+        Mat dst = frame;
+
+        if (capture.read(frame)) {
+
+            if (argc < 3)
+                resize(frame, baseWindow, { 320,200 });
+            else
+                resize(frame, baseWindow, { atoi(argv[1]), atoi(argv[2]) });
+           
+            putText(frame, "The HSV values: lows: {0,10,60}, highs: {20,150,255}", { 0,20 }, FONT_HERSHEY_PLAIN, 1.0, { 255,4,4,4 });
+
+            GaussianBlur(baseWindow, dst, Size(5, 5), 0);
+            imshow("GaussianBlur", dst);
+        }
+        else {
+            capturing = false;
+        }
+
+        cvtColor(frame, HSV, COLOR_BGR2HSV);
+        cvtColor(frame, hsvRange, COLOR_BGR2HSV);
+        inRange(hsvRange,
+            Scalar(lowRanges[0], lowRanges[1], lowRanges[2]),
+            Scalar(upRanges[0], upRanges[1], upRanges[2]),
+            hsvRange);
+
+        imshow("MyRange", hsvRange);
+        imshow("HSV", HSV);
+
+        if (waitKey(1) == 'x') {
+            Mat frameROI;
+            frameROI = frame.clone();
+            auto roi = selectROI("Use ROI on this window", frameROI);
+            Mat roiField = frameROI(roi);
+            imshow("ROI", roiField);
+            imwrite("photo.jpg", roiField);
+        }
+
+    } while (capturing);
+
+    return 0;
+}
