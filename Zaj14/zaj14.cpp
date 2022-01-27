f8d6fbcba5f02081d18f5353ff630a6371f80e2c
Mikolaj <nogamikolaj00@gmail.com>
2022-01-27 11:21:12 +0100

    add loading string from file to ex 14

--- Zaj14/zaj14.cpp
+++ Zaj14/zaj14.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <sstream>
 #include "iostream"
 #include "vector"
 #include "string"
@@ -36,23 +38,50 @@ pair<string, bool> decide(vector<string> decisionToMake){
     }
 }
 
+vector<string> splitText(const string& text){
+    char space_char = ' ';
+    vector<string> words{};
+
+    stringstream sstream(text);
+    string word;
+    while (std::getline(sstream, word, space_char)){
+        word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+vector<vector<string>> loadData(const string& path){
+    string data;
+    vector<vector<string>> decisionsToMake;
+    ifstream MyReadFile(path);
+    while (getline (MyReadFile, data)) {
+        decisionsToMake.push_back(splitText(data));
+    }
+
+    return decisionsToMake;
+}
+
 int main(int argc, char* argv[]){
-    vector<vector<string>> decisionsToMake = {
-            {"D1", "Sunny", "Hot", "High", "Weak"},
-            {"D2", "Sunny", "Hot", "High", "Strong"},
-            {"D3", "Overcast", "Hot", "High", "Weak"},
-            {"D4", "Rain", "Mild", "High", "Weak"},
-            {"D5", "Rain", "Cool", "Normal", "Weak"},
-            {"D6", "Rain", "Cool", "Normal", "Strong"},
-            {"D7", "Overcast", "Cool", "Normal", "Strong"},
-            {"D8", "Sunny", "Mild","High", "Weak"},
-            {"D9", "Sunny", "Cool", "Normal", "Weak"},
-            {"D10", "Rain", "Mild", "Normal", "Weak"},
-            {"D11", "Sunny", "Mild", "Normal", "Strong"},
-            {"D12", "Overcast", "Mild", "High", "Strong"},
-            {"D13", "Overcast", "Hot","Normal", "Weak"},
-            {"D14", "Rain", "Mild", "High", "Strong"}
-    };
+//    vector<vector<string>> decisionsToMake = {
+//            {"D1", "Sunny", "Hot", "High", "Weak"},
+//            {"D2", "Sunny", "Hot", "High", "Strong"},
+//            {"D3", "Overcast", "Hot", "High", "Weak"},
+//            {"D4", "Rain", "Mild", "High", "Weak"},
+//            {"D5", "Rain", "Cool", "Normal", "Weak"},
+//            {"D6", "Rain", "Cool", "Normal", "Strong"},
+//            {"D7", "Overcast", "Cool", "Normal", "Strong"},
+//            {"D8", "Sunny", "Mild","High", "Weak"},
+//            {"D9", "Sunny", "Cool", "Normal", "Weak"},
+//            {"D10", "Rain", "Mild", "Normal", "Weak"},
+//            {"D11", "Sunny", "Mild", "Normal", "Strong"},
+//            {"D12", "Overcast", "Mild", "High", "Strong"},
+//            {"D13", "Overcast", "Hot","Normal", "Weak"},
+//            {"D14", "Rain", "Mild", "High", "Strong"}
+//    };
+
+    vector<vector<string>> decisionsToMake = loadData(argv[1]);
 
     pair<string, bool> decision;
     for (auto &item : decisionsToMake){
